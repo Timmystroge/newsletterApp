73525ada6501762f51a982bd00718079aa509236
timmystroge <timmystroge75@gmail.com>
2023-03-28 21:28:41 +0100

    Setting up mvc

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const appController = require("controller/appController");
 // const bodyParser = require("body-parser");
 
 // set up body parser
@@ -9,9 +10,7 @@ const app = express();
 // app.use(express.bodyParser({ extended: true }));
 app.use(express.static("public"));
 
-
-
-
+appController(app);
 
 //listen to server
 app.listen(3000, function () {
---
+++ controller/appController.js
@@ -0,0 +1,5 @@
+const fireAppController = function (app) {
+  console.log("hello from controller");
+};
+
+module.exports = fireAppController;
