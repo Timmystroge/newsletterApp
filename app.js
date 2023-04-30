53c89d13764b4a7c034f3c2d525f42374d2c4594
timmystroge <timmystroge75@gmail.com>
2023-04-30 22:01:48 +0100

    Insert to newsletter database done

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 // set app
 const app = express();
+
+//connect to database
+mongoose
+  .connect("mongodb://127.0.0.1:27017/newsletterDB")
+  .then(function () {
+    console.log("connection Secured!");
+  })
+  .catch(function () {
+    console.log("Failed to connect");
+  });
+
+//create database schema
+const newsletterSchema = new mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  email: String,
+});
+
+//create database model
+const Newsletter = mongoose.model("Newsletter", newsletterSchema);
 
 // set middle wares
 app.set("view engine", "ejs");
@@ -18,12 +39,27 @@ app.get("/", function (req, res) {
 
 // handle post request
 app.post("/", urlencodedparser, function (req, res) {
-  console.log(req.body);
-  res.sendFile(`${__dirname}/success.html`);
+  const { firstname, lastname, email } = req.body;
+
+  //create insertion js object
+  const newNewsletter = new Newsletter({
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+  });
+
+  newNewsletter
+    .save()
+    .then(function () {
+      res.render("success");
+    })
+    .catch(function () {
+      res.render("failure", { data: "Failed to join newsletter" });
+    });
 });
 
 app.get("/*", (req, res) => {
-  res.sendFile(`${__dirname}/failure.html`);
+  res.render("failure", { data: "Whoops! Something went wrong" });
 });
 
 //listen to server
